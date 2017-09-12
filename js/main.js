405c85ed956b1adbb0577f6abdc3baa7f3089116
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-12 21:51:53 +0100

    added inital code for button pressing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 	console.log('The DOM is ready!');
 
-	var myButton = document.getElementById('my-button');
+	var display = document.getElementsByClassName('display');
+	var operatorPressed = false;
+	var num1;
+	var num2;
+	var calculation;
 
-	myButton.addEventListener('click', function (event){
+	var numButtons = document.getElementsByClassName('number');
+	for (var i = 0; i < numButtons.length; i++) {
+	    numButtons[i].addEventListener('click', function (event) {
+	        number = this.value;
+	        if (operatorPressed === false) {
+	        	num1 = number;
+	        	display[0].value = num1;
+	        } else {
+	        	num2 = number;
+	        	display[0].value = num1;
+	        }
+	    });
+	}
 
-		console.log('Help me! I\'ve been clicked!');
-
-	});
-
+	var numOperators = document.getElementsByClassName('operator');
+	for (var i = 0; i < numOperators.length; i++) {
+	    numOperators[i].addEventListener('click', function (event) {
+		    operator = this.value;
+		    if (num1 !== undefined) {
+		    calculation = operator;
+		    operatorPressed = true;
+		    }
+	    });
+	}
 });
